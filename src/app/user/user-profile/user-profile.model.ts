b5f0d4fc1fabe91380c403f710930d436ea71b0a
Dan Atkins <atkinsdj@chop.edu>
2019-12-03 17:29:35 -0500

    Updated auth to handle invalid registration and logins. Updated user profile to include components required for monitoring dashboard, etc. Added debug.log to .gitignore

--- src/app/user/user-profile/user-profile.model.ts
+++ src/app/user/user-profile/user-profile.model.ts
@@ -4,29 +4,48 @@
 //     providedIn: 'root'
 //   })
 export class UserProfile {
-    public userID : string;
-    public userName: string;
+    public badges=  { 'daily_survey': [],
+                     'money': 0,
+                     };
+    public daily_streak ={};
+    public imei:string = "0";
+    // had change below replacing - with _
+    public life_insights = {"daily_survey": {}};
+    public reinfrocement_data = {};
+
+
+
+    public lastupdate: number;
+    public readable_ts: string;
+    public survey_data = {
+        'daily_survey':{}, 
+        'points': 0,
+        'weekly_survey':{}
+    };
+    public username: string;
     public isParent: boolean;
     public isActive: boolean;
-    public datesTaken: number[];
+    public datesTaken: string[];
     public points: number;
     public badgeCount: number;
     //access token
     //refresh token 
 
-    constructor(userID: string, 
-                userName: string, 
+    constructor(username: string, 
                 isParent: boolean, 
-                datesTaken: number[], 
+                datesTaken: string[], 
                 points: number,
-                badgeCount: number){        
-        this.userID = userID;
+                badgeCount: number,
+                lastupdate: number,
+                readable_ts: string){        
+        // this.userID = userID;
         this.isParent = isParent;
-        this.userName = userName;
+        this.username = username;
         this.datesTaken = datesTaken;
         this.points = points;
         this.badgeCount = badgeCount;
-
+        this.lastupdate = lastupdate;
+        this.readable_ts = readable_ts;
     }
 }
     
