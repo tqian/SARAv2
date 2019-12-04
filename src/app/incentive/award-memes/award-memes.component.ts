72c7bfb33ef06862d73b72d3dd9896bae4baa103
Dan Atkins <atkinsdj@chop.edu>
2019-12-04 16:45:26 -0500

    Updated award-memes component with Mash's updates. Created environment.chop.ts (which I added to .gitignore) which is now referenced in the production configuration (in angular.json)

--- src/app/incentive/award-memes/award-memes.component.ts
+++ src/app/incentive/award-memes/award-memes.component.ts
@@ -1,19 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
+​
+import { GoogleAnalytics } from '@ionic-native/google-analytics/ngx';
+import { Router } from '@angular/router';
+​
 @Component({
   selector: 'app-award-memes',
   templateUrl: './award-memes.component.html',
   styleUrls: ['./award-memes.component.scss'],
 })
 export class AwardMemesComponent implements OnInit {
-
+​
   whichImage: string;
   //src="{{whichImage}}"
-  constructor() { }
-
+  constructor(private ga: GoogleAnalytics,
+    private router: Router) {
+  }
+​
   ngOnInit() {
+    this.ga.trackView('Life-insight')
+    .then(() => {console.log("trackView at Life-insight!")})
+    .catch(e => console.log(e));
+​
     var randomInt = Math.floor(Math.random() * 5) + 1;
     this.whichImage = "./assets/memes/"+randomInt+".jpg";
   }
-
+​
+  ratingChanged(rating){
+    if(rating==0)
+      console.log("thumbs down");
+    else
+      console.log("thumbs up");
+    
+    //this.router.navigate(['incentive/aquarium/aquariumone']);
+    //this.router.navigate(['/home']);
+    window.location.href = '/home';
+  }
+​
 }
